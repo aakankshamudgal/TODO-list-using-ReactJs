c86febaf922bef1ac0420d9ffa56e84cca96a045
aakankshamudgal <aakankshamudgal09@gmail.com>
2020-05-14 00:13:34 +0530

    Updates

---
+++ precache-manifest.165923b6bcd9cbdd43458e0c349d665f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14eff677caee8ef8042dc399cbeb4158",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3247e890016fc143493c",
+    "url": "/static/css/main.d67c52c4.chunk.css"
+  },
+  {
+    "revision": "6438c0237db83e29f453",
+    "url": "/static/js/2.2b460892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2b460892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3247e890016fc143493c",
+    "url": "/static/js/main.6c26955f.chunk.js"
+  },
+  {
+    "revision": "6301225ddfe152604189",
+    "url": "/static/js/runtime-main.8d8458cb.js"
+  }
+]);
